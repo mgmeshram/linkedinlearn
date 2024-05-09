af98a8533a3f994a4ee8d35e2509f5202cac7d2e
Mrunali Meshram <mrunali.meshram13@gmail.com>
2024-05-09 18:44:58 +0530

    Fixed the defect #324 for Addition syntax

---
+++ Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	
+		System.out.println("Addition of 7 and 3 is "+(7+3));
+
+	}
+
+}
