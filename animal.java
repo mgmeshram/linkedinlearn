1df5980c28d3e00a7517f78d5f8e8c9a5ad4339d
Mrunali Meshram <mrunali.meshram13@gmail.com>
2024-05-09 19:18:35 +0530

    Made changes in animal test cases to include reporting

---
+++ animal.java
@@ -0,0 +1,12 @@
+
+public class animal {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     void eat() {
+    	 System.out.println("eat");
+    	 
+     }
+	}
+
+
